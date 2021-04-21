13a707ace17cbbd89d951a324a7bdd4d424d0a7c
AMAL <mailtoamaldevcd@gmail.com>
2021-04-21 10:52:23 +0530

    annoying border removed

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 function show()
 
-{
+{   var x=document.getElementById("forTodo");
+    if(x.style.display=== "none")
+    {
+        x.style.display= "block";
+    }
+    // else
+    // {
+    //     x.style.display= "none";
+    // }
     var t1=document.getElementById("txt-form").value ;
     var link=document.createElement("li");
     link.classList.add("list-group-item");
