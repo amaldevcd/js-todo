56d5ca23555234e8a30241b830559f96ed85bb3b
AMAL <mailtoamaldevcd@gmail.com>
2021-04-21 13:02:04 +0530

    highlight

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ function show()
     var link=document.createElement("li");
     link.classList.add("list-group-item");
     link.innerHTML = t1;
+    link.style.cursor = "pointer";
     document.getElementById("list").appendChild(link);
+    link.addEventListener("click",function()
+    {
+        link.style.backgroundColor = "#6a757d";
+    })
     console.log(t1);
     document.getElementById("txt-form").value='' ;
+    
 }
